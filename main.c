ce6ccc59ed9b76f9577befaf911be2a46410c5d9
Alextryfg <114871727+Alextryfg@users.noreply.github.com>
2023-04-05 13:42:51 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "tabladesimbolos.h"
+#include "sintactico.h"
+#include "errores.h"
+
+int main(int argc, char **argv) {
+
+    //if(argc == 2){
+
+        /* Inicializamos el sistema de entrada */
+        init("C:\\Users\\USUARIO\\Desktop\\P2Compiladores\\CompiladorDFlex\\P2Compiladores\\regression.d");
+
+        /* Inicializamos la tabla de simbolos y la imprimimos por pantalla */
+        initTabla();
+        printTabla();
+
+        /* Empieza el analisis del sintactico que le pide componentes al analizador lexico */
+        compilarD();
+
+        /* Imprimimos la tabla de nuevo */
+        printTabla();
+
+        /* Liberamos la memoria de la tabla */
+        destruirTabla();
+
+        /* Cerramos el archivo regression.d */
+        cerrarD();
+
+    //}else{
+      //  errorD(5);
+    //}
+
+
+}
