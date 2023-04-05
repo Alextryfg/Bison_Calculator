b862230ace5ea49d253dbefea732f66ce545a72c
Alextryfg <114871727+Alextryfg@users.noreply.github.com>
2023-04-05 14:34:14 +0200

    Add files via upload

---
+++ definiciones.h
@@ -0,0 +1,24 @@
+#ifndef DEFINICIONS_H_
+#define DEFINITIONS_H_
+
+// Palabras reservadas e Identificadores
+#define IMPORT 300
+#define ID 301
+#define INT 303
+#define FLOAT 304
+#define WHILE 305
+#define FOREACH 306
+#define RETURN 307
+#define VOID 308
+#define CAST 309
+#define DOUBLE 310
+
+//TIPO DE DATO
+#define INTEGER 311
+#define FLOATPOINT 312
+#define STRING 302
+
+//Operadores
+#define OPERADOR 403
+
+#endif /* DEFINITIONS_H_ */
