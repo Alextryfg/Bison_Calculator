2a294e9c15fa881fae6d5bbef78431c655e5a237
Alextryfg <114871727+Alextryfg@users.noreply.github.com>
2023-04-12 17:39:05 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,31 @@
 
 int main(int argc, char **argv) {
 
-    //valgrind --leak-check=full --show-leak-kinds=all  -s ./calculadora
     //Mensaje de bienvenida
     welcome();
+
     //Se inicializa la tabla de simbolos
     initTablaSimbolos();
+
+    if(argc == 2){
+        int result = load(argv[1]);
+        if(result == 0){
+            return 1;
+        }
+    }
+
+    if(argc > 3){
+        printf("Demasiados argumentos.\n");
+        printf("Uso: ./calculadora <fichero>\n");
+        return 0;
+    }
+
     //Se comienza con el analisis
     yyparse();
     //Destruimos la tabla de simbolos
     destruirTablaSimbolos();
-    printf("Tabla de simbolos destruida correctamente\n");
+    return 1;
+
+
 
 }
